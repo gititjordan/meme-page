0fd7f9cf9cc55b7985bba26a43147aebf358ea7a
Andrew Trujillo <andrew.s.trujillo@gmail.com>
2022-04-28 15:42:28 -0700

    save javascript

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -1,55 +1,56 @@
-let currIndex = 0;
-let direction = 0; 
-function next() {
-    let urlExt = url.concat("/memetable/").concat((currIndex+1).toString())
-    let success = false;
-    if (currIndex >= 98) {
-        currIndex = 0
-    }
-    axios.get(urlExt, headers).then((res) => {
-                document.getElementById("image1").src=res.data.memeModel.url
-                success = true
-                console.log(res)
-            });
+// let x;
+// let currIndex = 0;
+// let direction = 0; 
+// function next() {
+//     let urlExt = url.concat("/memetable/").concat((currIndex+1).toString())
+//     let success = false;
+//     if (currIndex >= 98) {
+//         currIndex = 0
+//     }
+//     axios.get(urlExt, headers).then((res) => {
+//                 document.getElementById("image1").src=res.data.memeModel.url
+//                 success = true
+//                 console.log(res)
+//             });
     
-    currIndex++
-    direction = 1;
-}
-function previous() {
-    let urlExt = url.concat("/").concat((currIndex-1).toString())
-    if (currIndex <= 0) {
-        currIndex = 97
-    }
-    axios.get(urlExt, headers).then((res) => {
-        document.getElementById("image1").src=res.data.memeModel.url
-        currIndex-=1;
-    });
-    direction = -1;
-}
+//     currIndex++
+//     direction = 1;
+// }
+// function previous() {
+//     let urlExt = url.concat("/").concat((currIndex-1).toString())
+//     if (currIndex <= 0) {
+//         currIndex = 97
+//     }
+//     axios.get(urlExt, headers).then((res) => {
+//         document.getElementById("image1").src=res.data.memeModel.url
+//         currIndex-=1;
+//     });
+//     direction = -1;
+// }
 
-function random() {
-    let urlExt = url.concat("/memetable/")
-    axios.get(urlExt, headers).then((res) => {
-        document.getElementById("image1").src=res.data.memeModel.url
-        currIndex = res.data.memeModel.numInDb
-        console.log(res)
-    });
-    direction = 0;
-}
-function saved() {
-    let urlExt = url.concat( "/memelikedlist/").concat(userEmail).concat("/").concat(document.getElementById("image1").src);
-      axios.post(urlExt, headers)
-      .then((res) => {
-        console.log(res);
-        window.location.reload();
-      })
-}
-function handleError() {
-   if (direction < 0) {
-       previous()
-   } else if (direction > 0) {
-       next()
-   } else {
-       random()
-   }
-}
+// function random() {
+//     let urlExt = url.concat("/memetable/")
+//     axios.get(urlExt, headers).then((res) => {
+//         document.getElementById("image1").src=res.data.memeModel.url
+//         currIndex = res.data.memeModel.numInDb
+//         console.log(res)
+//     });
+//     direction = 0;
+// }
+// function save() {
+//     let urlExt = url.concat( "/memelikedlist/addmeme");
+//     let memeObj = {
+//         "email": "asadasdsa@gmail.com",
+//         "url": document.getElementById("image1").src
+//     }
+//     axios.post(urlExt, memeObj, headers).then((res) => {console.log(res.data)})
+// }
+// function handleError() {
+//    if (direction < 0) {
+//        previous()
+//    } else if (direction > 0) {
+//        next()
+//    } else {
+//        random()
+//    }
+// }
--- frontend/js/saved.js
+++ frontend/js/saved.js
@@ -1,14 +1,54 @@
-let memelikedlist = ['https://knowyourmeme.com/photos/2186254', 'https://www.youtube.com/watch?v=lxMIqyN0HJ4', 'https://knowyourmeme.com/photos/2276133', 'https://www.reddit.com/r/memes/comments/t43hif/itz_non_of_dein_busines/', 'https://m.facebook.com/Rmemes0/photos/the-price-of…in-by-mateus-figueredorashnutin/2526490427403233/', 'https://www.pinterest.com/pin/611082243172242709/', 'https://starwars.fandom.com/f/p/4400000000003176703', 'https://www.reddit.com/r/terriblefacebookmemes/com…from_rmemes_which_is_basically_facebook_memes_at/', 'https://www.youtube.com/watch?v=sSpDnIJxyXM', 'https://www.facebook.com/Rmemes0/', 'https://www.pinterest.com/pin/12173861481542868/', 'https://knowyourmeme.com/photos/2207649', 'https://www.youtube.com/watch?v=3NxTbAxWxqA', 'https://twitter.com/memesofredditt/status/1425349920559611906', 'https://jojo.fandom.com/f/p/4400000000000001221/r/4400000000000019281', 'https://www.reddit.com/r/memes/comments/qyx1hw/ooh_thats_right/', 'https://omghotmemes.tumblr.com/post/180523596787/doing-the-left-thing-via-rmemes', 'https://art.branipick.com/found-on-rmemes/', 'https://knowyourmeme.com/photos/2222630', 'https://www.pinterest.com/pin/677791812664693515/', 'https://www.youtube.com/watch?v=BQz_JS1gjM0', 'https://www.clashofclanshacksadvice.com/2020/08/05/misc-found-on-r-memes/', 'https://cataclysmeme.wordpress.com/2019/06/16/saw-…thought-it-belonged-here-via-r-wholesomememes-15/', 'https://www.reddit.com/r/memes/comments/rgsg5v/youre_welcome_btw/', 'https://www.facebook.com/Rmemes0/', 'https://aminoapps.com/c/meme/page/blog/r-memes-and-r-me-irl/dXzx_pwibu1PaPX6GYob1wQ8WdQvwg7oNM', 'https://knowyourmeme.com/photos/2188867', 'https://www.youtube.com/watch?v=K1jWPl7pNKo', 'https://imgflip.com/i/62s8qf', 'https://www.reddit.com/r/memes/comments/sjcze7/like_seriously_what_is_that/', 'https://www.facebook.com/Rmemes0/', 'https://browsedankmemes.com/post/185477329386/from-rmemes-via-rwholesomememes', 'https://mobile.twitter.com/rstatsmemes', 'https://memebase.cheezburger.com/tag/r-dankmemes'];
-let currentPage = 1;
-let amountOfPages = memelikedlist / 8;
-// function load() {
-//     let urlExt = url.concat("/memelikedlist/").concat(userEmail);
-//         axios.get(urlExt, headers).then((res) => {
-//             memelikedlist = res.data.memeLikedList
-//     });
-// }
-function loadMemes() {
-    for (let index = 1; index < 9; index++) {
-        document.getElementById(index.toString()).src=memelikedlist[index]
+let currentPage = 0;
+let extra = memelikedlist.length % 8
+let amountOfPages = memelikedlist.length / 8 - 1;
+async function loadMemes() {
+    await load()
+    console.log(memelikedlist);
+    for (let index = 1; index <= 8; index++) {
+        document.getElementById("pic".concat(index.toString())).src=memelikedlist[index-1];
     }
 }
+
+function load(){
+    return new Promise((resolve,reject)=>{
+        let urlExt = url.concat("/memelikedlist/").concat(userEmail);
+        axios.get(urlExt, headers).then((res) => {
+            console.log(res)
+            memelikedlist = res.data.memeLikedList;
+        })
+        setTimeout(()=>{
+            resolve();
+        ;} , 5000
+        );
+    });
+}
+
+
+function next8() {
+    currentPage ++
+    if (currentPage <= amountOfPages) { 
+        for (let index = 0; index < 8; index++) {
+            document.getElementById("pic".concat(index.toString())).src=memelikedlist[index+(currentPage*8)];
+        }
+    } else if (currentPage <= amountOfPages + 1){
+        for (let index = 0; index < 8; index++) {
+            document.getElementById("pic".concat(index.toString())).src=memelikedlist[index+(currentPage*8)];
+            if(index >= extra) {
+                document.getElementById("pic".concat(index.toString())).src="";
+            }
+        }
+    } else {
+        currentPage--
+    }
+}
+function previous8() {
+    currentPage --
+    if (currentPage >= 0) { 
+        for (let index = 0; index < 8; index++) {
+            document.getElementById("pic".concat(index.toString())).src=memelikedlist[index+(currentPage*8)];
+        }
+    } else {
+        currentPage++
+    }
+}
+
